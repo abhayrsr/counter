3969f35536795a938106da648ca0c5a7ce14f127
abhay <abhayrajsinghrana123@gmail.com>
2023-06-26 21:26:34 +0530

    update: eventListener

---
+++ app.js
@@ -0,0 +1,51 @@
+var increase = document.querySelector("#increase");
+var reset = document.querySelector("#reset");
+var decrease = document.querySelector("decrease");
+
+increase.addEventListener("click", letIncrease);
+decrease.addEventListener("click", letDecrease);
+reset.addEventListener("click", letReset);
+
+
+
+function letIncrease(){
+    let number = document.getElementById("count").innerText;
+    console.log(number);
+    document.getElementById("count").innerHTML = `
+
+    <div id = "number">
+    <p id = "count"> ${(number*1) + 1} </p>
+    </div>
+    
+
+    `
+}
+
+
+function letDecrease(){
+    let number = document.getElementById("count").innerText;
+    console.log(number);
+    document.getElementById("count").innerHTML = `
+
+    <div id = "number">
+    <p id = "count"> ${(number*1) - 1} </p>
+    </div>
+    
+
+    `
+}
+
+
+function letReset(){
+    let number = document.getElementById("count").innerText;
+    console.log(number);
+    document.getElementById("count").innerHTML = `
+
+    <div id = "number">
+    <p id = "count"> ${0} </p>
+    </div>
+    
+
+    `
+}
+
